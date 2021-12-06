afb3c5b549f4c0d44e1974a9d4a370416727020c
Victor Wanderley <victor.wanderley1709@gmail.com>
2021-12-06 19:20:28 -0300

    First Commit, initial files.

---
+++ src/test/java/com/boo/personapi/PersonapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boo.personapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
