24fdbd4902809b7a5c0314e88ee91c315e2093da
Victor Wanderley <victor.wanderley1709@gmail.com>
2022-01-09 18:12:52 -0300

    Creating requests DTO's.

---
+++ src/main/java/com/boo/personapi/dto/request/PersonDTO.java
@@ -0,0 +1,41 @@
+package com.boo.personapi.dto.request;
+
+import com.boo.personapi.entity.Phone;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CPF;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PersonDTO {
+
+    private Long id;
+
+    @NotEmpty
+    @Size(min = 2, max = 70)
+    private String firstName;
+
+    @NotEmpty
+    @Size(min = 2, max = 70)
+    private String lastName;
+
+    @NotEmpty
+    @CPF
+    private String cpf;
+
+    private String birthDate;
+
+    @Valid
+    @NotEmpty
+    private List<Phone> phone;
+}
