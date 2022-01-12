e17d00b3ab9b3d6c07d2a07218a49602681f7849
Victor Wanderley <victor.wanderley1709@gmail.com>
2022-01-11 22:09:53 -0300

    Modify Person's service and controller constructors.

--- src/main/java/com/boo/personapi/controllers/PersonController.java
+++ src/main/java/com/boo/personapi/controllers/PersonController.java
@@ -5,6 +5,7 @@ import com.boo.personapi.dto.request.PersonDTO;
 import com.boo.personapi.entity.Person;
 import com.boo.personapi.exception.PersonNotFoundException;
 import com.boo.personapi.services.PersonService;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -14,13 +15,9 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/person")
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonController {
     private final PersonService service;
-
-    @Autowired
-    public PersonController(PersonService service) {
-        this.service = service;
-    }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
--- src/main/java/com/boo/personapi/services/PersonService.java
+++ src/main/java/com/boo/personapi/services/PersonService.java
@@ -6,6 +6,7 @@ import com.boo.personapi.entity.Person;
 import com.boo.personapi.exception.PersonNotFoundException;
 import com.boo.personapi.mappers.PersonMapper;
 import com.boo.personapi.repository.PersonRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,13 +14,10 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonService {
     private final PersonRepository repository;
     private final PersonMapper personMapper = PersonMapper.INSTANCE;
-    @Autowired
-    public PersonService(final PersonRepository repository) {
-        this.repository = repository;
-    }
 
     public MessageResponseDTO createPerson(final PersonDTO personDTO) {
         Person saved = repository.save(personMapper.personDTOtoPerson(personDTO));
